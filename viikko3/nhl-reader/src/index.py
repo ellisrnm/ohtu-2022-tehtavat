758a218a2315512649a6d9c60268cd002c41356b
Elli Saarenmaa <47243975+ellisrnm@users.noreply.github.com>
2022-01-29 22:37:04 +0200

    Refactoring nhl-reader

--- viikko3/nhl-reader/src/index.py
+++ viikko3/nhl-reader/src/index.py
@@ -1,30 +1,13 @@
-import requests
-from player import Player
+from player import PlayerReader, PlayerStats
 
 def main():
     url = "https://nhlstatisticsforohtu.herokuapp.com/players"
-    response = requests.get(url).json()
+    reader = PlayerReader(url)
+    stats = PlayerStats(reader)
+    players = stats.top_scorers_by_nationality("FIN")
 
-    players = []
-
-    for player_dict in response:
-        player = Player(
-            player_dict['name'],
-            player_dict['nationality'],
-            player_dict['team'],
-            player_dict['goals'],
-            player_dict['assists']
-        )
-
-        players.append(player)
-
-    players.sort(key=lambda player: player.goals+player.assists, reverse=True)
-
-    print("Players from Finland:\n")
-    
     for player in players:
-        if player.nationality == 'FIN':
-            print(player)
+        print(player)
 
 if __name__ == "__main__":
     main()
--- viikko3/nhl-reader/src/player.py
+++ viikko3/nhl-reader/src/player.py
@@ -1,4 +1,5 @@
 from re import template
+import requests
 
 
 class Player:
@@ -11,3 +12,33 @@ class Player:
     
     def __str__(self):
         return f'{self.name:20} {self.team:3} {self.goals:2} + {self.assists:2} = {self.goals+self.assists:2}'
+
+class PlayerReader:
+    def __init__(self, url):
+        self.url = url
+        self.players = []
+
+    def get_players(self):
+        response = requests.get(self.url).json()
+
+        for player_dict in response:
+            player = Player(
+                player_dict['name'],
+                player_dict['nationality'],
+                player_dict['team'],
+                player_dict['goals'],
+                player_dict['assists']
+            )
+
+            self.players.append(player)
+        return self.players
+
+class PlayerStats:
+    def __init__(self, reader):
+        self.reader = reader
+        self.players = reader.get_players()
+
+    def top_scorers_by_nationality(self, nationality):
+        players = [player for player in self.players if player.nationality==nationality]
+        players.sort(key=lambda player: player.goals+player.assists, reverse=True)
+        return players
