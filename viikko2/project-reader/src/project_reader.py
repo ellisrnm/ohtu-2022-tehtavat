935c7b59702774c64d67c27331d62087130eeb00
Elli Saarenmaa <47243975+ellisrnm@users.noreply.github.com>
2022-01-29 17:16:08 +0200

    Parse toml

---
+++ viikko2/project-reader/src/project_reader.py
@@ -0,0 +1,19 @@
+from urllib import request
+from project import Project
+from toml import loads
+
+class ProjectReader:
+    def __init__(self, url):
+        self._url = url
+
+    def get_project(self):
+        # tiedoston merkkijonomuotoinen sisältö
+        content = request.urlopen(self._url).read().decode("utf-8")
+        parsed = loads(content)["tool"]["poetry"]
+        name = parsed["name"]
+        description = parsed["description"]
+        dep = parsed["dependencies"]
+        dep_dev = parsed["dev-dependencies"]
+
+        # deserialisoi TOML-formaatissa oleva merkkijono ja muodosta Project-olio sen tietojen perusteella
+        return Project(name, description, dep, dep_dev)
