8e049f425de1a2b9807c5d65c3b12a16b4e796e7
Elli Saarenmaa <47243975+ellisrnm@users.noreply.github.com>
2022-01-29 22:00:23 +0200

    Print players list

--- viikko3/nhl-reader/src/index.py
+++ viikko3/nhl-reader/src/index.py
@@ -1,7 +1,28 @@
+import requests
 from player import Player
 
 def main():
-    pass
+    url = "https://nhlstatisticsforohtu.herokuapp.com/players"
+    response = requests.get(url).json()
+
+    players = []
+
+    for player_dict in response:
+        player = Player(
+            player_dict['name'],
+            player_dict['nationality'],
+            player_dict['team'],
+            player_dict['goals'],
+            player_dict['assists']
+        )
+
+        players.append(player)
+
+    print("Players from Finland:\n")
+
+    for player in players:
+        if player.nationality == 'FIN':
+            print(player)
 
 if __name__ == "__main__":
     main()
