062d85f8342cff47a2eaf30a0db2e4b90c5874a8
Elli Saarenmaa <47243975+ellisrnm@users.noreply.github.com>
2022-02-12 17:13:55 +0200

    step5: price for two products

--- viikko4/tdd-ostoskori/src/tests/ostoskori_test.py
+++ viikko4/tdd-ostoskori/src/tests/ostoskori_test.py
@@ -5,24 +5,27 @@ from tuote import Tuote
 class TestOstoskori(unittest.TestCase):
     def setUp(self):
         self.kori = Ostoskori()
+        self.maito = Tuote("Maito", 3)
+        self.kahvi = Tuote("Kahvi", 7)
 
     def test_ostoskorin_hinta_ja_tavaroiden_maara_alussa(self):
         self.assertEqual(self.kori.hinta(), 0)
         self.assertEqual(self.kori.tavaroita_korissa(), 0)
 
     def test_yhden_tuotteen_lisaamisen_jalkeen_korissa_yksi_tavara(self):
-        maito = Tuote("Maito", 3)
-        self.kori.lisaa_tuote(maito)
+        self.kori.lisaa_tuote(self.maito)
         self.assertEqual(self.kori.tavaroita_korissa(), 1)
 
     def test_yhden_tuotteen_lisaamisen_jalkeen_korin_hinta_tuotteen_hinta(self):
-        maito = Tuote("Maito", 3)
-        self.kori.lisaa_tuote(maito)
+        self.kori.lisaa_tuote(self.maito)
         self.assertEqual(self.kori.hinta(), 3)
 
     def test_kahden_tuotteen_lisaamisen_jalkeen_korissa_kaksi_tavaraa(self):
-        maito = Tuote("Maito", 3)
-        kahvi = Tuote("Kahvi", 7)
-        self.kori.lisaa_tuote(maito)
-        self.kori.lisaa_tuote(kahvi)
+        self.kori.lisaa_tuote(self.maito)
+        self.kori.lisaa_tuote(self.kahvi)
         self.assertEqual(self.kori.tavaroita_korissa(), 2)
+
+    def test_kahden_tuotteen_lisaamisen_jalkeen_hinta_kahden_tuotteen_hintojen_summa(self):
+        self.kori.lisaa_tuote(self.maito)
+        self.kori.lisaa_tuote(self.kahvi)
+        self.assertEqual(self.kori.hinta(), 10)
